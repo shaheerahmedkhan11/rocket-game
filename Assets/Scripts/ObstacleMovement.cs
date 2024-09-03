e8fc816e7196708ccd588d8315636ba2af9b2095
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-09-03 15:53:15 +0500

    Environment enhanced
    Nothing

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -7,10 +7,12 @@ public class ObstacleMovement : MonoBehaviour
     [SerializeField]
     float duration = 5f;
     public Transform startPoint, endPoint;
+    MeshRenderer meshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-       
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = true;
     }
 
     // Update is called once per frame
