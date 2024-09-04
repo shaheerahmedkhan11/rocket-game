a0104abffd627abac767ac67e13845084abebbd6
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-09-04 22:44:12 +0500

    Game completed!

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -17,6 +17,8 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= 0)
+            return;
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2;
         float rawSineWave = Mathf.Sin(cycles * tau);
---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            Debug.Log("You have quit the application");
+        }
+            
+    }
+}
